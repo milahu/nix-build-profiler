13cb9a9b25e94d78b5ca0245a8461f1fff85c113
Milan Hauth <milahu@gmail.com>
2022-06-24 18:44:56 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,42 @@
+{ lib
+, buildPythonPackage
+, psutil
+, prefixed
+, writeText
+}:
+
+buildPythonPackage rec {
+  pname = "nix-build-profiler";
+  version = "0.0.1";
+
+  src = ./.;
+
+  propagatedBuildInputs = [
+    psutil
+    prefixed
+  ];
+
+  setupHook = writeText "setup-hook.sh" ''
+    startNixBuildProfiler() {
+      echo "Starting nix-build-profiler"
+      nix-build-profiler &
+    }
+    prePhases+=" startNixBuildProfiler"
+  '';
+
+  meta = with lib; {
+    description = "Profile CPU and memory usage of nix-build";
+    longDescription = ''
+      Usage:
+
+      ```nix
+      mkDerivation {
+        nativeBuildInputs = [ nix-build-profiler ];
+      }
+      ```
+    '';
+    homepage = "https://github.com/milahu/nix-build-profiler";
+    license = licenses.mit;
+    maintainers = with maintainers; [ milahu ];
+  };
+}
