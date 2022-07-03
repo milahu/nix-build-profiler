40c33787957c03cde60a2f236ae148fbcf01ea8c
Milan Hauth <milahu@gmail.com>
2022-07-03 14:57:32 +0200

    fix format

--- nix_build_profiler.py
+++ nix_build_profiler.py
@@ -156,7 +156,7 @@ def print_process_info(
     #print(f"\n{'load':<{cpu_width}s} mem rss  vms  proc @ {t}", file=file)
     #print(f"\n{'load':<{cpu_width}s} mem rss  Ncp ncp  proc @ {t}", file=file)
     #print(f"\n{'load':<{cpu_width}s}  rss spr cpr proc @ {t}", file=file)
-    print(f"\n{'load':<{cpu_width}s}  {'load1':<{cpu_width}s} rss tim spr cpr proc @ {t}", file=file)
+    print(f"\n{'load':<{cpu_width}s} {'Load':<{cpu_width}s} rss  tim spr cpr proc @ {t}", file=file)
     #print(f"\n{'load':<{cpu_width}s} mem proc @ {t}", file=file)
     # spr = sum of all child processes, including self
     # cpr = number of first child processes, excluding transitive children
@@ -244,12 +244,12 @@ def print_process_info(
     name = exe
   name = os.path.basename(name)
 
-  total_time = info['total_time'] / 60.0 # time in minutes
+  total_time = float(info['total_time']) / 60.0 # time in minutes
 
   #print(f"{sum_cpu:{cpu_width}.1f} {sum_mem:3.0f} {Float(sum_rss):4.0h} {sum_ncp:3d} {ncp:3d} {depth*indent}{name}{info_str}", file=file)
   #print(f"{sum_cpu:{cpu_width}.1f} {sum_ncp:3d} {Float(sum_rss):4.0h} {ncp:3d} {depth*indent}{name}{info_str}", file=file)
   #print(f"{sum_cpu:{cpu_width}.1f} {(sum_rss / mebi):4.0f} {sum_ncp:3d} {ncp:3d} {depth*indent}{name} {pid}: {cmdline_str}{info_str}", file=file)
-  print(f"{info['total_load']:{cpu_width}.1f} {sum_cpu:{cpu_width}.1f} {total_time:3.0f} {(sum_rss / mebi):4.0f} {sum_ncp:3d} {ncp:3d} {depth*indent}{name} {pid}: {cmdline_str}{info_str}", file=file)
+  print(f"{sum_cpu:{cpu_width}.1f} {info['total_load']:{cpu_width}.1f} {(sum_rss / mebi):4.0f} {total_time:3.0f} {sum_ncp:3d} {ncp:3d} {depth*indent}{name} {pid}: {cmdline_str}{info_str}", file=file)
 
   if config_print_env_vars:
     for k in info["environ"]:
